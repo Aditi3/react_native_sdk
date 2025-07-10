e3680ca209f86acc18d2012cbc8e14701633f444
Aditi3 <Aditi3@users.noreply.github.com>
2025-07-10 10:28:34 +0200

    feat: update version number to 5.4.1

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ Adjust.getLastDeeplink = function(callback) {
 };
 
 Adjust.getSdkVersion = function(callback) {
-    module_adjust.getSdkVersion("react-native5.4.0", callback);
+    module_adjust.getSdkVersion("react-native5.4.1", callback);
 };
 
 Adjust.componentWillUnmount = function() {
@@ -329,7 +329,7 @@ Adjust.teardown = function(testParam) {
 
 var AdjustConfig = function(appToken, environment) {
     // common
-    this.sdkPrefix = "react-native5.4.0";
+    this.sdkPrefix = "react-native5.4.1";
     this.appToken = appToken;
     this.environment = environment;
     this.logLevel = null;
