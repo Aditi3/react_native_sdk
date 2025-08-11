6a12cae5271ae4ae89a6eb02c0bbb709f8d1f969
uerceg <ugi@adjust.com>
2025-08-12 01:14:25 +0200

    feat: update version number to 5.4.2

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ Adjust.getLastDeeplink = function(callback) {
 };
 
 Adjust.getSdkVersion = function(callback) {
-    module_adjust.getSdkVersion("react-native5.4.1", callback);
+    module_adjust.getSdkVersion("react-native5.4.2", callback);
 };
 
 Adjust.componentWillUnmount = function() {
@@ -329,7 +329,7 @@ Adjust.teardown = function(testParam) {
 
 var AdjustConfig = function(appToken, environment) {
     // common
-    this.sdkPrefix = "react-native5.4.1";
+    this.sdkPrefix = "react-native5.4.2";
     this.appToken = appToken;
     this.environment = environment;
     this.logLevel = null;
